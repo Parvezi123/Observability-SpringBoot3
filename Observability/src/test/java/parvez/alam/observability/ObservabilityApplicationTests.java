fca88a48aa6009bb1023e16a6e9118895f5678f9
Parvez Alam <parvezimohammed@gmail.com>
2023-04-07 21:41:45 +0530

    Initial Commit

---
+++ Observability/src/test/java/parvez/alam/observability/ObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package parvez.alam.observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
