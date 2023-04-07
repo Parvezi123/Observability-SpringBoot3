88da28c5f7761e1ebc4eb1f4178a9fe6052c41f6
Parvez Alam <parvezimohammed@gmail.com>
2023-04-07 22:26:11 +0530

    Added ObservationHandler

---
+++ Observability/src/main/java/parvez/alam/observability/handler/SimpleLoggingHandler.java
@@ -0,0 +1,47 @@
+package parvez.alam.observability.handler;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SimpleLoggingHandler implements ObservationHandler<Observation.Context> {
+    @Override
+    public void onStart(Observation.Context context) {
+        log.info("Start, context Name : {}", context.getName());
+    }
+
+    @Override
+    public void onStop(Observation.Context context) {
+        log.info("Stop, context Name : {}", context.getName());
+    }
+
+    @Override
+    public void onScopeOpened(Observation.Context context) {
+        log.info("ScopeOpened, context Name : {}", context.getName());
+    }
+
+    @Override
+    public void onScopeClosed(Observation.Context context) {
+        log.info("ScopeClosed, context Name : {}", context.getName());
+    }
+
+    @Override
+    public void onError(Observation.Context context) {
+
+        var error = context.getError();
+        var message = error != null ? error.getMessage() : null ;
+
+        log.info("Error Occurred, context Name : {} , ; Error : {}", context.getName(), message);
+    }
+
+    @Override
+    public void onEvent(Observation.Event event, Observation.Context context) {
+        log.info("EVENT, context Name : {}   ;  event Name : {}", context.getName(), event.getName());
+    }
+
+    @Override
+    public boolean supportsContext(Observation.Context context) {
+        return true;
+    }
+}
